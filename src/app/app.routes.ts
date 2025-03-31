ab6ac4802d83f9c6ded2519dccb5ca286bf6bb95
Angxs20 <l.teaa030819@itses.edu.mx>
2025-03-31 00:14:40 -0600

    Esqueleto de angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'registro', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', component: DashbordComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirige al login por defecto
+  ];
